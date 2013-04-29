d14d4fe81cff485946e38bd46323b843753c459c
Yannick Eberhardt <Yannick.Eberhardt@web.de>
2013-04-29 14:41:58 +0200

    added kunderesourcetest and abstractresourcetest

---
+++ shop/src/test/java/de/shop/kundenverwaltung/rest/KundeResourceTest.java
@@ -0,0 +1,52 @@
+package de.shop.kundenverwaltung.rest;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import de.shop.util.AbstractResourceTest;
+
+@RunWith(Arquillian.class)
+public class KundeResourceTest extends AbstractResourceTest{
+
+	@Test
+	public void validate() {
+		assertThat(true, is(true));
+	}
+	
+	@Ignore
+	@Test
+	public void notYetImplemented() {
+		fail();
+	}
+	
+	@Test
+	public void findKundeById() {
+		LOGGER.finer("BEGINN");
+		
+		// Given
+		final Long kundeId = Long.valueOf(1);
+		
+		// When
+		final Response response = given().header(ACCEPT, APPLICATION_JSON)
+				                         .pathParameter(KUNDEN_ID_PATH_PARAM, kundeId)
+                                         .get(KUNDEN_ID_PATH);
+
+		// Then
+		assertThat(response.getStatusCode(), is(HTTP_OK));
+		
+		try (final JsonReader jsonReader =
+				              getJsonReaderFactory().createReader(new StringReader(response.asString()))) {
+			final JsonObject jsonObject = jsonReader.readObject();
+			assertThat(jsonObject.getJsonNumber("id").longValue(), is(kundeId.longValue()));
+		}
+		
+		LOGGER.finer("ENDE");
+	}
+	
+}
---
+++ shop/src/test/java/de/shop/util/AbstractResourceTest.java
@@ -0,0 +1,70 @@
+package de.shop.util;
+
+import java.util.Locale;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.Archive;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+
+
+public abstract class AbstractResourceTest {
+	protected static final String USERNAME = "102";
+	protected static final String PASSWORD = "102";
+	protected static final String USERNAME_ADMIN = "101";
+	protected static final String PASSWORD_ADMIN = "101";
+	protected static final String PASSWORD_FALSCH = "falsch";
+	
+	// Sprache und Zeichensatz
+	private static final String CLIENT_LANGUAGE = Locale.GERMAN.getLanguage();
+	private static final String UTF_8 = "UTF-8";
+	
+	private static JsonReaderFactory jsonReaderFactory;    // Fabrik zum Lesen bei GET-Requests
+	private static JsonBuilderFactory jsonBuilderFactory;  // Fabrik zum Schreiben bei POST- und PUT-Requests
+	
+	
+	@Deployment(name = ArchiveBuilder.TEST_WAR, testable = false)  // Die Tests laufen nicht im Container
+	protected static Archive<?> deployment() {
+		return ArchiveBuilder.getInstance().getArchive();
+	}
+	
+	
+	@BeforeClass
+	public static void init() {
+		jsonReaderFactory = Json.createReaderFactory(null);
+		jsonBuilderFactory = Json.createBuilderFactory(null);
+	}
+	
+	@Before
+	public void before() {
+		// Basispfad und Port
+		RestAssured.baseURI = BASEURI;
+		RestAssured.port = PORT;
+		RestAssured.basePath = BASEPATH;
+		
+		final List<Header> httpClientHeaders = new ArrayList<>();
+		httpClientHeaders.add(new BasicHeader("Accept-Language", CLIENT_LANGUAGE));
+		
+		// UTF-8 statt Latin-1
+		RestAssured.config = newConfig().encoderConfig(encoderConfig().defaultContentCharset(UTF_8))
+				                        .decoderConfig(decoderConfig().defaultContentCharset(UTF_8))
+				                        .httpClient(httpClientConfig().setParam(DEFAULT_HEADERS, httpClientHeaders));
+	}
+	
+	
+	@After
+	public void reset() {
+		RestAssured.reset();
+	}
+
+
+	protected static JsonReaderFactory getJsonReaderFactory() {
+		return jsonReaderFactory;
+	}
+
+	protected static JsonBuilderFactory getJsonBuilderFactory() {
+		return jsonBuilderFactory;
+	}
+}
