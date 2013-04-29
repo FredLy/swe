e1bc46697503b4ec198762626a2fa2ec8c3c4bb1
Yannick Eberhardt <Yannick.Eberhardt@web.de>
2013-04-29 15:46:25 +0200

    edited kunderesource

--- shop/src/test/java/de/shop/kundenverwaltung/rest/KundeResourceTest.java
+++ shop/src/test/java/de/shop/kundenverwaltung/rest/KundeResourceTest.java
@@ -1,21 +1,34 @@
 package de.shop.kundenverwaltung.rest;
 
+import static com.jayway.restassured.RestAssured.given;
+import static de.shop.util.TestConstants.ACCEPT;
+import static de.shop.util.TestConstants.KUNDEN_ID_PATH_PARAM;
+import static de.shop.util.TestConstants.KUNDEN_ID_PATH;
+import static java.net.HttpURLConnection.HTTP_OK;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+
+import java.io.StringReader;
+import java.lang.invoke.MethodHandles;
+import java.util.logging.Logger;
+
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.json.JsonObject;
-import javax.json.JsonReader;
-
+import com.jayway.restassured.response.Response;
 import de.shop.util.AbstractResourceTest;
 
 @RunWith(Arquillian.class)
 public class KundeResourceTest extends AbstractResourceTest{
+	
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 
 	@Test
 	public void validate() {
